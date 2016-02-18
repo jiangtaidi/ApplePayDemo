9bf91484569ddd06c77baa073e3255b0126d1722
jiangtaidi <jiangtd@jiangtddeMacBook-Air.local>
2016-02-18 21:14:59 +0800

    first commit

--- ApplePayDemo/ViewController.m
+++ ApplePayDemo/ViewController.m
@@ -14,14 +14,89 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)applePay:(id)sender
+{
+    if([PKPaymentAuthorizationViewController canMakePayments])
+    {
+        PKPaymentRequest *paymentRequest = [[PKPaymentRequest alloc] init];
+        paymentRequest.countryCode = @"US";
+        paymentRequest.currencyCode = @"USD";
+        paymentRequest.supportedNetworks = @[PKPaymentNetworkAmex, PKPaymentNetworkMasterCard, PKPaymentNetworkVisa];
+        paymentRequest.merchantCapabilities = PKMerchantCapabilityEMV;
+        paymentRequest.merchantIdentifier = @"merchant.com.applePay.jtd";
+        
+        PKPaymentSummaryItem *widget1 = [PKPaymentSummaryItem summaryItemWithLabel:@"product 1" amount:[NSDecimalNumber decimalNumberWithString:@"0.99"]];
+        
+        PKPaymentSummaryItem *widget2 = [PKPaymentSummaryItem summaryItemWithLabel:@"product 2" amount:[NSDecimalNumber decimalNumberWithString:@"1.00"]];
+        
+        PKPaymentSummaryItem *total = [PKPaymentSummaryItem summaryItemWithLabel:@"Grand Total" amount:[NSDecimalNumber decimalNumberWithString:@"1.99"]];
+        
+        paymentRequest.paymentSummaryItems = @[widget1, widget2, total];
+        
+        PKPaymentAuthorizationViewController *paymentPane = [[PKPaymentAuthorizationViewController alloc] initWithPaymentRequest:paymentRequest];
+        paymentPane.delegate = self;
+        [self presentViewController:paymentPane animated:TRUE completion:nil];
+    }
+    else
+    {
+        NSLog(@"current device cannot support applePay");
+    }
 }
+
+#pragma mark ========xx=====
+
+- (void)paymentAuthorizationViewController:(PKPaymentAuthorizationViewController *)controller
+                       didAuthorizePayment:(PKPayment *)payment
+                                completion:(void (^)(PKPaymentAuthorizationStatus status))completion
+{
+    NSLog(@"Payment was authorized: %@", payment);
+    
+    // do an async call to the server to complete the payment.
+    // See PKPayment class reference for object parameters that can be passed
+    BOOL asyncSuccessful = NO;
+    
+    // When the async call is done, send the callback.
+    // Available cases are:
+    //    PKPaymentAuthorizationStatusSuccess, // Merchant auth'd (or expects to auth) the transaction successfully.
+    //    PKPaymentAuthorizationStatusFailure, // Merchant failed to auth the transaction.
+    //
+    //    PKPaymentAuthorizationStatusInvalidBillingPostalAddress,  // Merchant refuses service to this billing address.
+    //    PKPaymentAuthorizationStatusInvalidShippingPostalAddress, // Merchant refuses service to this shipping address.
+    //    PKPaymentAuthorizationStatusInvalidShippingContact        // Supplied contact information is insufficient.
+    
+    if(asyncSuccessful) {
+        completion(PKPaymentAuthorizationStatusSuccess);
+        
+        // do something to let the user know the status
+        
+        NSLog(@"Payment was successful");
+        
+        //        [Crittercism endTransaction:@"checkout"];
+        
+    } else {
+        completion(PKPaymentAuthorizationStatusFailure);
+        
+        // do something to let the user know the status
+        
+        NSLog(@"Payment was unsuccessful");
+        
+        //        [Crittercism failTransaction:@"checkout"];
+    }
+
+}
+
+- (void)paymentAuthorizationViewControllerDidFinish:(PKPaymentAuthorizationViewController *)controller
+{
+    NSLog(@"Finishing payment view controller");
+    
+    // hide the payment window
+    [controller dismissViewControllerAnimated:TRUE completion:nil];
+}
+
 
 @end
